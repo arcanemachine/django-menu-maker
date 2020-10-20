7d6645da914f4e09b3db55a9092b403e5c5439ce
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-20 11:58:12 -0600

    create new secret key that is not stored in the repo

--- menus_project/settings.py
+++ menus_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import keys
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$zm_+yth!x&vr6$#j5#19llq^9nfj7oa=i%!7!$nt@mo18fvw&'
+SECRET_KEY = keys.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

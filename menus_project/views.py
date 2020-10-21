df4c7fad3200152dfd124af77c2c7bf04d5263c4
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-21 13:57:50 -0600

    remove hello_world view

--- menus_project/views.py
+++ menus_project/views.py
@@ -1,9 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello_world(request):
-    return HttpResponse(f"Hello, {request.resolver_match.namespace}:"\
-            "{request.resolver_match.view_name}!")
-
 def root(request):
     return render(request, 'root.html')

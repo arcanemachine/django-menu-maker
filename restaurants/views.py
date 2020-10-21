ea075bff46489dd968308d94a602d6a1c8b29217
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-21 13:02:22 -0600

    create restaurant DetailView

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from .models import Restaurant
 
 class RestaurantListView(ListView):
     model = Restaurant
     context_object_name = 'restaurants'
+
+class RestaurantDetailView(DetailView):
+    model = Restaurant

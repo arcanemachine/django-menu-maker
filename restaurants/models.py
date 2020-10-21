9313e0c6f5b6bf12fb2f10d0f09b49b97ccbeea8
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-21 15:01:14 -0600

    move two-line statement into single line

--- restaurants/models.py
+++ restaurants/models.py
@@ -7,15 +7,13 @@ from django.utils.text import slugify
 class Restaurant(models.Model):
 
     name = models.CharField(max_length=128)
-    slug = models.SlugField(
-        max_length=128, null=True, unique=True)
+    slug = models.SlugField(max_length=128, unique=True, null=True)
     admin_users = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return self.name
 
     def clean(self):
-
         # do not allow duplicate restaurant slugs
         existing_restaurants_with_same_slug = \
             Restaurant.objects.filter(slug=slugify(self.name))

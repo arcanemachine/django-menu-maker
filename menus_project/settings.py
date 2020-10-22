a7f45288a37b427505834b58fd67f7b07b7f082d
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-22 01:47:43 -0600

    add user login/logout functionality

--- menus_project/settings.py
+++ menus_project/settings.py
@@ -127,3 +127,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Authentication
+
+LOGIN_REDIRECT_URL = '/'
--- menus_project/urls.py
+++ menus_project/urls.py
@@ -12,6 +12,18 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+django.contrib.auth views:
+	accounts/ register/ [name='register']
+	accounts/ login/ [name='login']
+	accounts/ logout/ [name='logout']
+	accounts/ password_change/ [name='password_change']
+	accounts/ password_change/done/ [name='password_change_done']
+	accounts/ password_reset/ [name='password_reset']
+	accounts/ password_reset/done/ [name='password_reset_done']
+	accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+	accounts/ reset/done/ [name='password_reset_complete']
+    
 """
 from django.contrib import admin
 from django.urls import include, path
@@ -20,6 +32,7 @@ from . import views
 
 urlpatterns = [
     path('', views.root, name='root'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurants.urls')),
     path('restaurant/<slug:restaurant_slug>/menu/', include('menus.urls')),

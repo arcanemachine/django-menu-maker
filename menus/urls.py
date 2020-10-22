2ed020418e636671e9d3acd1402f3e1fcee961a4
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-22 01:42:38 -0600

    remove RestaurantMenuListView URL

--- menus/urls.py
+++ menus/urls.py
@@ -5,9 +5,6 @@ from . import views
 app_name = 'menus'
 
 urlpatterns = [
-    path('',
-        views.RestaurantMenuListView.as_view(),
-        name='restaurant_menu_list'),
     path('<slug:menu_slug>/',
         views.MenuDetailView.as_view(),
         name='menu_detail'),

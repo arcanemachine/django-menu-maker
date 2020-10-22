dc5807fd8d7c3ab9a23c229e320183bc7bc0b1e2
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-22 01:41:38 -0600

    add MenuDetail view and template

--- menus/views.py
+++ menus/views.py
@@ -6,3 +6,9 @@ from restaurants.models import Restaurant
 
 class MenuDetailView(DetailView):
     model = Menu
+    slug_url_kwarg = 'menu_slug'
+
+    def get_object(self):
+        return Menu.objects.get(
+                restaurant__slug=self.kwargs['restaurant_slug'],
+                slug=self.kwargs['menu_slug'])

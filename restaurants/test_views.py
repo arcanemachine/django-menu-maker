cd6ea278ae9b6ad37f6856d6de9d2eac5e1f1094
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-21 23:39:11 -0600

    RestaurantDetailView: initial tests (get, view_type, context_object_type)

--- restaurants/test_views.py
+++ restaurants/test_views.py
@@ -34,6 +34,35 @@ class RestaurantListViewTest(TestCase):
         self.assertTrue('restaurants' in self.context)
 
     def test_context_object_type_is_Restaurant(self):
+        self.assertEqual(self.context['view'].model.__name__, 'Restaurant')
+
+class RestaurantDetailViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+
+        # create test user
+        cls.test_user = get_user_model().objects.create(username='test_user')
+        cls.test_user.set_password('password')
+        cls.test_user.save()
+
+        # create test restaurant
+        cls.test_restaurant = Restaurant.objects.create(name='Test Restaurant')
+        cls.test_restaurant.admin_users.set([cls.test_user])
+        cls.test_restaurant.save()
+
+    def setUp(self):
+        self.response = self.client.get(
+            reverse('restaurants:restaurant_detail', 
+                kwargs = {'restaurant_slug': self.test_restaurant.slug }))
+        self.context = self.response.context
+
+    def test_get(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_view_type_is_DetailView(self):
         self.assertEqual(
-            self.context['restaurants'].first().__class__.__name__,
-            'Restaurant')
+            self.context['view'].__class__.__bases__[0].__name__, "DetailView")
+
+    def test_context_object_type_is_Restaurant(self):
+        self.assertEqual(self.context['view'].model.__name__, 'Restaurant')

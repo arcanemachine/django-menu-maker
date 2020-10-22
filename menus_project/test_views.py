9fd059a1f310be3ad68576e3c5493f41d3ce949a
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-22 01:43:36 -0600

    root: basic tests complete

---
+++ menus_project/test_views.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class RootViewTest(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('root'))
+
+    def test_get(self):
+        self.assertTrue(self.response.status_code, 200)
+
+    def test_template_name(self):
+        self.assertTemplateUsed(self.response, 'root.html')

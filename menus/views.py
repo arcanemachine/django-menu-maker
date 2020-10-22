34ad61d1d7379ec30f8dedf0b810fa16f7805b31
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-22 01:40:11 -0600

    remove RestaurantMenuListView (duplicate functionality as restaurants:restaurant_detail

--- menus/views.py
+++ menus/views.py
@@ -4,23 +4,5 @@ from django.views.generic import DetailView, ListView
 from .models import Menu
 from restaurants.models import Restaurant
 
-class RestaurantMenuListView(ListView):
-    template_name = 'menus/restaurant_menu_list.html'
-    context_object_name = 'menus'
-
-    def dispatch(self, request, *args, **kwargs):
-        self.restaurant = \
-            get_object_or_404(Restaurant, slug=self.kwargs['restaurant_slug'])
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['restaurant'] = self.restaurant
-        return context
-
-    def get_queryset(self):
-        return Menu.objects.filter(
-                restaurant__slug=self.kwargs['restaurant_slug'])
-
 class MenuDetailView(DetailView):
     model = Menu

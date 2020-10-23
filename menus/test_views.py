df9bfeb13f8cdd9035a37b74e9c5cb9f0854ab7f
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-23 02:02:05 -0600

    MenuSectionCreateViewTest: unauthorized user tests

--- menus/test_views.py
+++ menus/test_views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+from urllib.parse import urlparse
+
 from .models import Menu, MenuSection
 from restaurants.models import Restaurant
 
@@ -153,5 +155,52 @@ class MenuSectionCreateViewTest(TestCase):
     def test_view_template_name(self):
         self.assertEqual(self.context['view'].template_name,
                 'menus/menusection_create.html')
+
+    # get_context_data
+    def test_view_context_contains_menu(self):
+        self.assertTrue('menu' in self.context)
+
+    def test_view_context_contains_correct_menu(self):
+        self.assertEqual(self.context['menu'], self.test_menu)
+
+    # authentication - unauthorized user
+    def test_view_get_method_unauthenticated_user(self):
+        self.client.logout()
+        self.response = self.client.get(self.current_test_url, follow=True)
+        self.context = self.response.context
+        self.html = self.response.content.decode('utf-8')
+
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/login.html')
+
+    def test_view_get_method_authenticated_but_unauthorized_user(self):
+        self.client.login(
+                username='test_user', password='password')
+        self.response = self.client.get(self.current_test_url)
+        self.context = self.response.context
+        self.html = self.response.content.decode('utf-8')
+
+        self.assertEqual(self.response.status_code, 403)
+
+    def test_view_post_method_unauthenticated_user(self):
+        self.client.logout()
+
+        # get menusection count before attempting to post data
+        old_menusection_count = MenuSection.objects.count()
+
+        # attempt to create new menusection via POST
+        self.response = self.client.post(self.current_test_url,
+                kwargs = {'name': 'Test Menu Section'})
+
+        # new menusection count should be the same as before
+        new_menusection_count = MenuSection.objects.count()
+        self.assertEqual(old_menusection_count, new_menusection_count)
+
+        # user is redirected to login page
+        self.assertEqual(self.response.status_code, 302)
+
+        # URL must be parsed to account for GET parameters
+        redirect_url = urlparse(self.response.url)[2]
+        self.assertEqual(redirect_url, reverse('login'))
 
 

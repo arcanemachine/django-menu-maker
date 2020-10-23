5450e68fbe63ebc118d8b3cc47f9c0bb3f7188d2
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-22 22:37:36 -0600

    replace hyphen in str(object) with a colon, to save space

--- menus/models.py
+++ menus/models.py
@@ -59,7 +59,7 @@ class MenuSection(models.Model):
     slug = models.SlugField(max_length=128, null=True)
 
     def __str__(self):
-        return f"{self.menu.restaurant.name} - {self.menu.name} - {self.name}"
+        return f"{self.menu.restaurant.name}: {self.menu.name} - {self.name}"
 
     def get_absolute_url(self):
         return reverse('menus:menusection_detail',

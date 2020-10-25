0f6bcce9083cc6a17113f906c4f02744fd568af2
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-25 17:06:14 -0600

    add MenuItem to admin interface

--- menus/admin.py
+++ menus/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Menu, MenuSection
+from .models import Menu, MenuSection, MenuItem
 
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
@@ -16,3 +16,11 @@ class MenuSectionAdmin(admin.ModelAdmin):
 
     class Meta:
         model = MenuSection
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+
+    class Meta:
+        model = MenuItem
+

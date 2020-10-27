2991669926a0829e771b18588b5b3d3241d8cd6f
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-27 01:33:11 -0600

    fix indentaion

--- menus_project/urls.py
+++ menus_project/urls.py
@@ -14,16 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 
 django.contrib.auth views:
-	accounts/ register/ [name='register']
-	accounts/ login/ [name='login']
-	accounts/ logout/ [name='logout']
-	accounts/ password_change/ [name='password_change']
-	accounts/ password_change/done/ [name='password_change_done']
-	accounts/ password_reset/ [name='password_reset']
-	accounts/ password_reset/done/ [name='password_reset_done']
-	accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
-	accounts/ reset/done/ [name='password_reset_complete']
-    
+    accounts/ register/ [name='register']
+    accounts/ login/ [name='login']
+    accounts/ logout/ [name='logout']
+    accounts/ password_change/ [name='password_change']
+    accounts/ password_change/done/ [name='password_change_done']
+    accounts/ password_reset/ [name='password_reset']
+    accounts/ password_reset/done/ [name='password_reset_done']
+    accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+    accounts/ reset/done/ [name='password_reset_complete']
+
 """
 from django.contrib import admin
 from django.urls import include, path

7a46d009d1e59d8ac477c9d0bf0c889763705a8b
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-31 14:33:59 -0600

    Add URL Pattern for LogoutView

--- users/urls.py
+++ users/urls.py
@@ -26,6 +26,10 @@ urlpatterns = [
         auth_views.LoginView.as_view(),
         name='login'),
     path(
+        'logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'),
+    path(
         'register/',
         views.RegisterView.as_view(),
         name='register'),

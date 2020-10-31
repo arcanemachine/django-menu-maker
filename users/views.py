867b8a5f96de0e6f95ab708547b6173d8fff51c3
Nicholas Moen <arcanemachine@tutanota.com>
2020-10-31 14:34:46 -0600

    RegisterView: Add success_message

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-class RegisterView(CreateView):
+
+class RegisterView(SuccessMessageMixin, CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy(settings.LOGIN_URL)
     template_name = 'registration/register.html'
+    success_url = reverse_lazy(settings.LOGIN_URL)
+    success_message = "Account successfully registered"

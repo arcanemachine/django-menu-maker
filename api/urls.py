c17b2f6d357e61f46bc729e67af1c306d7b0eb3b
Nicholas Moen <arcanemachine@tutanota.com>
2020-11-07 00:52:23 -0700

    Restaurant: use ViewSet and SimpleRouter

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
 
 from . import views
 
 app_name = 'api'
 
-urlpatterns = [
-    path('', views.RestaurantList.as_view()),
-    ]
+router = SimpleRouter()
+router.register('', views.RestaurantViewSet, basename='restaurants')
+
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import viewsets
 
 from .serializers import RestaurantSerializer
+from .permissions import HasRestaurantPrivileges
 from restaurants.models import Restaurant
 
-class RestaurantList(generics.ListCreateAPIView):
+class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer

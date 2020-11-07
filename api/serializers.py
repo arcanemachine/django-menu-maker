d0b0596a569cda4698be6140c54d660fcfabc549
Nicholas Moen <arcanemachine@tutanota.com>
2020-11-07 16:14:53 -0700

    Menu: Rearrange fields

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from restaurants.models import Restaurant
 from menus.models import Menu, MenuSection, MenuItem
 
+
 class RestaurantSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -10,11 +11,15 @@ class RestaurantSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'admin_users', 'menu_set']
         read_only_fields = ['admin_users', 'menu_set']
 
+
 class MenuSerializer(serializers.ModelSerializer):
 
     restaurant_name = serializers.ReadOnlyField(source='restaurant.name')
 
     class Meta:
         model = Menu
-        fields = ['id', 'name', 'restaurant_name', 'menusection_set']
-        read_only_fields = ['name', 'menusection_set']
+        fields = ['id', 'name', 'restaurant', 'restaurant_name',
+            'menusection_set']
+        read_only_fields = ['restaurant', 'menusection_set']
+
+

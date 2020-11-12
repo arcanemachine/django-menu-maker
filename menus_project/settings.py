68b8d599150ec07d73276f4ad9fb0101041b4486
Nicholas Moen <arcanemachine@tutanota.com>
2020-11-12 12:12:30 -0700

    Add SessionAuthentication to rest_framework auth params

--- menus_project/settings.py
+++ menus_project/settings.py
@@ -129,6 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [

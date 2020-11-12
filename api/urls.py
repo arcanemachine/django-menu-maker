ff77568ae301240805429cecd67c1fa3a57a4ce4
Nicholas Moen <arcanemachine@tutanota.com>
2020-11-12 01:00:12 -0700

    Make API URL structure human-parsable

--- api/urls.py
+++ api/urls.py
@@ -1,33 +1,39 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
 app_name = 'api'
 
 urlpatterns = [
-    path('',
+    path('api-token-auth/',
+         obtain_auth_token,
+         name='api_token_auth'),
+    path('restaurants/',
          views.RestaurantList.as_view(),
          name='restaurant_list'),
-    path('<int:restaurant_pk>/',
+    path('restaurants/<int:restaurant_pk>/',
          views.RestaurantDetail.as_view(),
          name='restaurant_detail'),
-    path('<int:restaurant_pk>/m/',
+    path('restaurants/<int:restaurant_pk>/menus/',
          views.MenuList.as_view(),
          name='menu_list'),
-    path('<int:restaurant_pk>/m/<int:menu_pk>/',
+    path('restaurants/<int:restaurant_pk>/menus/<int:menu_pk>/',
          views.MenuDetail.as_view(),
          name='menu_detail'),
-    path('<int:restaurant_pk>/m/<int:menu_pk>/s/',
+    path('restaurants/<int:restaurant_pk>/menus/<int:menu_pk>/sections/',
          views.MenuSectionList.as_view(),
          name='menusection_list'),
-    path('<int:restaurant_pk>/m/<int:menu_pk>/s/<int:menusection_pk>/',
+    path('restaurants/<int:restaurant_pk>/menus/<int:menu_pk>/sections/'
+         '<int:menusection_pk>/',
          views.MenuSectionDetail.as_view(),
          name='menusection_detail'),
-    path('<int:restaurant_pk>/m/<int:menu_pk>/s/<int:menusection_pk>/i/',
+    path('restaurants/<int:restaurant_pk>/menus/<int:menu_pk>/sections/'
+         '<int:menusection_pk>/items/',
          views.MenuItemList.as_view(),
          name='menuitem_list'),
-    path('<int:restaurant_pk>/m/<int:menu_pk>/s/<int:menusection_pk>/i/'
-         '<int:menuitem_pk>/',
+    path('restaurants/<int:restaurant_pk>/menus/<int:menu_pk>/sections/'
+         '<int:menusection_pk>/items/<int:menuitem_pk>/',
          views.MenuItemDetail.as_view(),
          name='menuitem_detail'),
     ]

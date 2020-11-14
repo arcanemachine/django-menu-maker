312e9ac48cb096417d325a0008d6326d5a4c06b4
Nicholas Moen <arcanemachine@tutanota.com>
2020-11-13 22:21:07 -0700

    Use context processor to ensure navbar links contain proper redirects

--- menus_project/context_processors.py
+++ menus_project/context_processors.py
@@ -1,5 +1,12 @@
+from django.urls import reverse
+
 from .constants import PROJECT_NAME
 
 
 def project_name(request):
     return {'PROJECT_NAME': PROJECT_NAME}
+
+
+def common_urls(request):
+    return {'user_login_url': reverse('users:login'),
+            'user_register_url': reverse('users:register')}

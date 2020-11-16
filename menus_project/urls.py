cf8bb5acb24b52dde294c2557d69a201e3d2528c
Nicholas Moen <arcanemachine@tutanota.com>
2020-11-15 17:53:55 -0700

    (minor) Remove useless docstrings from urls.py

--- menus_project/urls.py
+++ menus_project/urls.py
@@ -1,16 +1,3 @@
-"""
-django.contrib.auth views:
-    accounts/ register/ [name='register']
-    accounts/ login/ [name='login']
-    accounts/ logout/ [name='logout']
-    accounts/ password_change/ [name='password_change']
-    accounts/ password_change/done/ [name='password_change_done']
-    accounts/ password_reset/ [name='password_reset']
-    accounts/ password_reset/done/ [name='password_reset_done']
-    accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
-    accounts/ reset/done/ [name='password_reset_complete']
-
-"""
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,3 @@
-"""
-django.contrib.auth views:
-
-    register/  'register'                          users.RegisterView
-    login/  'login'                                LoginView
-    logout/  'logout'                              LogoutView
-    password_change/  'password_change'            PasswordChangeForm
-    password_change/done/  'password_change_done'  PasswordChangeDoneView
-    password_reset/  'password_reset'              PasswordResetView
-    password_reset/done/  'password_reset_done'    PasswordResetDoneView
-    reset/<uidb64>/<token>/ password_reset_confirm PasswordResetConfirmView
-    reset/done/  'password_reset_complete'         PasswordResetCompleteView
-"""
 from django.contrib.auth import views as auth_views
 from django.urls import path
 

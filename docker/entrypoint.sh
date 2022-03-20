53ecb1dee04b8acd0848cdb024ac2395d6cf3268
Nicholas Moen <arcanemachine@tutanota.com>
2022-03-20 03:36:58 -0600

    Remove unnecessary config

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,53 +1,8 @@
 #!/bin/bash
 
-# example config taken from: https://github.com/siddharthsahu/django-docker
-
 set -o errexit
 set -o pipefail
 set -o nounset
-
-# postgres_ready() {
-#     python << END
-# import sys
-# from psycopg2 import connect
-# from psycopg2.errors import OperationalError
-# try:
-#     connect(
-#         dbname="${DJANGO_POSTGRES_DATABASE}",
-#         user="${DJANGO_POSTGRES_USER}",
-#         password="${DJANGO_POSTGRES_PASSWORD}",
-#         host="${DJANGO_POSTGRES_HOST}",
-#         port="${DJANGO_POSTGRES_PORT}",
-#     )
-# except OperationalError:
-#     sys.exit(-1)
-# END
-# }
-#
-# redis_ready() {
-#     python << END
-# import sys
-# from redis import Redis
-# from redis import RedisError
-# try:
-#     redis = Redis.from_url("${CELERY_BROKER_URL}", db=0)
-#     redis.ping()
-# except RedisError:
-#     sys.exit(-1)
-# END
-# }
-#
-# until postgres_ready; do
-#   >&2 echo "Waiting for PostgreSQL to become available..."
-#   sleep 5
-# done
-# >&2 echo "PostgreSQL is available"
-#
-# until redis_ready; do
-#   >&2 echo "Waiting for Redis to become available..."
-#   sleep 5
-# done
-# >&2 echo "Redis is available"
 
 echo "PROJECT_NAME_PYTHON is set to: '$PROJECT_NAME_PYTHON'"
 echo "PROJECT_PORT_INTERNAL is set to: '$PROJECT_PORT_INTERNAL'"
